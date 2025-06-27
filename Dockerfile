45838a4e34bd9751895ec7fd30557a5997d2680a
fuzz <hyeonminmo@hanyang.ac.kr>
2025-06-27 14:42:15 +0900

    implementation - init.sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM fuzzer_base/aflgo as aflgo
-FROM fuzzer_basse/windranger as windranger
+FROM fuzzer_base/windranger as windranger
 
 FROM dcfuzz_bench/aflgo as bench_aflgo
 FROM dcfuzz_bench/windranger as bench_windranger
@@ -19,11 +19,41 @@ ENV DEBIAN_FRONTEND=noninteractive \
     LANG=C.UTF-8
 
 
+# Install proper tools
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    build-essential cmake git curl wget unzip \
+    autoconf automake libtool bison flex \
+    zlib1g-dev libssl-dev python3 python3-pip \
+    llvm clang clang-format clang-tidy \
+    ninja-build pkg-config lcov python3-setuptools \
+    python3-dev libglib2.0-dev libxml2-dev \
+    libncurses5-dev libsqlite3-dev \
+    tzdata sudo vim tmux htop zsh
+
+
+# Copy fuzzer image
+
+COPY --chown=$UID:$GID --from=aflgo /fuzzer /fuzzer
+COPY --chown=$UID:$GID --from=windranger /fuzzer /fuzzer
+
+
+# Copy program with each fuzzer image 
+
+COPY --chown=$UID:$GID --from=bench_aflgo /benchmark/bin /benchmark/bin
+COPY --chown=$UID:$GID --from=bench_windranger /benchmark/bin /benchmark/bin
 
 
 
+USER root
 
 
+# install newer python3 
+RUN apt install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev tk-dev ca-certificates
+
+
+WORKDIR /root
 
 
 
--- dcfuzz/main.py
+++ dcfuzz/main.py
@@ -1,12 +1,133 @@
+import atexit
+import copy
+import datetime
+import json
+import logging
+import math
+import os
+import pathlib
+import random
+import signal
+import subprocess
+import sys
+import threading
+import time
+import traceback
+from abc import abstractmethod
+from collections import deque
+from pathlib import Path
+from typing import Deque, Dict, List, Optional
+
+if __package__ is None:
+    sys.path.append(
+        os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+    __package__ = "dcfuzz"
+
+from cgroupspy import trees
+
+#from . import cgroup_utils, cli
+from . import config as Config
+#from . import coverage, fuzzer_driver, fuzzing, policy, sync, utils
+#from .common import IS_DEBUG, IS_PROFILE, nested_dict
+#from .datatype import Bitmap
+#from .mytype import BitmapContribution, Coverage, Fuzzer, Fuzzers
+#from .singleton import SingletonABCMeta
+
+
+
+
+config: Dict = Config.CONFIG
 
 
 logger = logging.getLogger('dcfuzz.main')
 
 
+LOG = nested_dict()
+
+OUTPUT: Path
+INPUT : Optional[Path]
+LOG_DATETIME: str
+LOG_FILE_NAME: str
+
 TARGET: str
 FUZZERS: Fuzzers = []
+CPU_ASSIGN: Dict[Fuzzer, float] = {}
+
 ARGS: cli.ArgsParser
 
+
+START_TIME: float = 0.0
+
+SLEEP_GRANULARITY: int = 60
+
+RUNNING: bool = False
+
+
+
+def cleanup(exit_code=0):
+    global ARGS
+    logger.info('cleanup')
+    LOG['end_time'] = time.time()
+    write_log()
+    for fuzzer in FUZZERS:
+        stop(fuzzer)
+    if exit_code == 0 and ARGS.tar:
+        save_tar()
+    os._exit(exit_code)
+
+
+def cleanup_exception(etype, value, tb):
+    traceback.print_exception(etype, value, tb)
+    cleanup(1)
+
+# An initialization function that prepares the execution environment before fuzzing by setting up termination and exception handling, creating a health check file, and initializing the logging structure.
+def init():
+    global START_TIME, LOG
+    signal.signal(signal.SIGTERM, lambda x, frame: sys.exit(0))
+    signal.signal(signal.SIGINT, lambda x, frame: sys.exit(0))
+    atexit.register(cleanup)
+    sys.excepthook = cleanup_exception
+    health_check_path = os.path.realpath(os.path.join(ARGS.output, 'health'))
+    pathlib.Path(health_cehck_path).touch(mode=0o666, exist_ok=True)
+    LOG['log'] = []
+    LOG['round'] = []
+
+
+
+def init_cgroup():
+    '''
+    cgroup /dcfuzz is created by /init.sh, the command is the following:
+
+    cgcreate -t yufu -a yufu -g cpu:/autofz
+    '''
+    global FUZZERS, CGROUP_ROOT
+    cgroup_path = cgroup_utils.get_cgroup_path()
+    container_id = os.path.basename(cgroup_path)
+    cgroup_path_fs = os.path.join('/sys/fs/cgroup/cpu', cgroup_path[1:])
+    dcfuzz_cgroup_path_fs = os.path.join(cgroup_path_fs, 'dcfuzz')
+
+    if not os.path.exists(dcfuzz_cgroup_path_fs):
+        logger.critical(
+            'dcfuzz cgroup not exists. make sure to run /init.sh first')
+        terminate_autofz()
+
+    t = trees.Tree()
+    p = os.path.join('/cpu', cgroup_path[1:], 'dcfuzz')
+    CGROUP_ROOT = os.path.join(cgroup_path, 'dcfuzz')
+
+    cpu_node = t.get_node_by_path(p)
+
+    for fuzzer in FUZZERS:
+        fuzzer_cpu_node = t.get_node_by_path(os.path.join(p, fuzzer))
+
+        if not fuzzer_cpu_node:
+            fuzzer_cpu_node = cpu_node.create_cgroup(fuzzer)
+
+        cfs_period_us = fuzzer_cpu_node.controller.cfs_period_us
+        quota = int(cfs_period_us * (JOBS))
+        fuzzer_cpu_node.controller.cfs_quota_us = quota
+
+    return True
 
 
 
@@ -34,6 +155,47 @@ def main():
             exit(1)
 
     try:
+        os.makedirs(OUTPUT, exist_ok=False)
+    except FileExistsError:
+        logger.error(f'remove {OUTPUT{')
+        exit(1)
+    with open(os.path.join(OUTPUT, 'cmdline', 'w') as f:
+        cmdline = " ".join(sys.argv)
+        LOG['cmd'] = cmdline
+        f.write(f"{cmdline}\n")
+
+    init()
+
+    current_time = time.time()
+
+
+    # set the run-time for each phase
+    SYNC_TIME = ARGS.sync
+
+
+    timeout = ARGS.timeout
+
+
+    # evaluate fuzzer
+    # ?
+
+    START_TIME = time.time()
+
+    init_cgroup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
